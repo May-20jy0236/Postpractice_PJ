fd54576f5213963acabd9994a004ed8f143b15cd
May <mayzinaung19950921@gmail.com>
2022-06-14 16:54:47 +0900

    Initial Commit

---
+++ Postpractice_PJ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Postpractice_PJ
+//
+//  Created by HL株式会社 on 14/6/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
